0f5fbfbff9a7e74c727716058b2fbf826058093d
Sabique <sabique@live.com>
2018-03-30 11:52:32 +0530

    Changes

--- public/student_registration_processing.php
+++ public/student_registration_processing.php
@@ -1,23 +1,24 @@
-yc<?php require_once("../includes/connect.php");
+<?php require_once("../includes/connect.php");
 	  require_once("../includes/functions.php");	?>
 
 <?php
 
 	# Retreving data from form
 	# user_input_validation() is used to check whether user input is safe or not
+
 	$name 			= user_input_validation($_REQUEST["name"]);
 	$dob 			= user_input_validation($_REQUEST["dob"]);
 	$gender 		= user_input_validation($_REQUEST["sex"]);
 	$father 		= user_input_validation($_REQUEST["fname"]);
 	$f_occupation 	= user_input_validation($_REQUEST["f_occupation"]);
-	$mother 		= user_input_validation($_REQUEST["mname"];
-	$m_occupation 	= user_input_validation( $_REQUEST["m_occupation"]);
+	$mother 		= user_input_validation($_REQUEST["mname"]);
+	$m_occupation 	= user_input_validation($_REQUEST["m_occupation"]);
 	$religion 		= user_input_validation($_REQUEST["religion"]);
 	$caste 			= user_input_validation($_REQUEST["caste"]);
-	$category 		= user_input_validation( $_REQUEST["category"]);
+	$category 		= user_input_validation($_REQUEST["category"]);
 	$blood 			= user_input_validation($_REQUEST["bgroup"]);
-	$aadhar 		= user_input_validation( $_REQUEST["aadhar"]);
-	$housename 		= user_input_validation( $_REQUEST["hname"];
+	$aadhar 		= user_input_validation($_REQUEST["aadhar"]);
+	$housename 		= user_input_validation($_REQUEST["hname"]);
 	$place 			= user_input_validation($_REQUEST["plc"]);
 	$postoffice 	= user_input_validation($_REQUEST["post"]);
 	$district 		= user_input_validation($_REQUEST["district"]);
@@ -25,18 +26,18 @@ yc<?php require_once("../includes/connect.php");
 	$email 			= user_input_validation($_REQUEST["email"]);
 	$yoa 			= user_input_validation($_REQUEST["yoa"]); #Year of Admission
 	$admission_no 	= user_input_validation($_REQUEST["admno"]);
-	$reg_no 		= user_input_validation( $_REQUEST["register"]);
+	$reg_no 		= user_input_validation($_REQUEST["register"]);
 	$course 		= user_input_validation($_REQUEST["course"]);
-	$semester	 	= user_input_validation( $_REQUEST["smstr"]);
+	$semester	 	= user_input_validation($_REQUEST["smstr"]);
 	$userid 		= user_input_validation($_REQUEST["UserID"]);
-	$password 		= user_input_validation( $_REQUEST["psw"]);
+	$password 		= user_input_validation($_REQUEST["psw"]);
 	$repeat_password= user_input_validation($_REQUEST["psw-repeat"]);
+
+	# Convert password into hash value.
+	$password = md5($password);
 
 	# Accessing connection variable form connect.php
 	global $conn;
-
-	$test;
-
 
 	# Command to insert into table student_info
 	$sql = "INSERT INTO student_info ( ";
@@ -46,6 +47,8 @@ yc<?php require_once("../includes/connect.php");
 	$sql.= "gender,";
 	$sql.= "father,";
 	$sql.= "f_occupation,";
+	$sql.= "mother,";
+	$sql.= "m_occupation,";
 	$sql.= "religion,";
 	$sql.= "caste,";
 	$sql.= "category,";
@@ -66,6 +69,7 @@ yc<?php require_once("../includes/connect.php");
 	$sql.= "password ";
 
 	$sql.= ") VALUES ( ";
+
 	$sql.= " '{$name}' , ";
 	$sql.= " '{$dob}' , ";
 	$sql.= " '{$gender}' , ";
@@ -93,6 +97,7 @@ yc<?php require_once("../includes/connect.php");
 	$sql.= " '{$password}' ";
 	$sql.= ")";
 
+	#Executing query
 	if (mysqli_query($conn, $sql)) {
 	    echo "New record created successfully";
 	} else {
